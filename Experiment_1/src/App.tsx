6612b7d587fb72cc53bd2db8075748e673a4265d
afsarzan <afsar_zan@yahoo.com>
2026-01-01 14:49:24 -0800

    Initialize React + TypeScript + Vite project
    Add base project files for a new React application using TypeScript and Vite, including configuration files, initial source code, ESLint setup, and documentation.

---
+++ Experiment_1/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import  ParentComponent  from './Components/Parent';
+
+function App() {
+
+  return (
+    <>
+      <ParentComponent />     
+    </>
+  )
+}
+
+export default App
---
+++ Experiment_1/src/Components/Parent.tsx
@@ -0,0 +1,6 @@
+
+export default function ParentComponent() {
+  return (
+    <div>Parent</div>
+  )
+}
